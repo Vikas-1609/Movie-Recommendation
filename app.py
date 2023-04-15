2c32f0f8ff5bc426fcb1606ac1f64d8f11dc7d63
vikas <vikassahu3936@gmail.com>
2023-04-15 10:53:33 +0530

    eleventh

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+# Load the pickled files
+movies = pickle.load(open("E:\\web projects\Movie Recommender Site\\movie_list.pkl", 'rb'))
+similarity = pickle.load(open("E:\\web projects\Movie Recommender Site\\similarity.pkl", 'rb'))
+
+# Create the Flask app
+app = Flask(__name__)
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    return recommended_movie_names
+
+# Define the route for the home page
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/recommend', methods=['POST'])
+def recommendation():
+    # Get the movie name entered by the user
+    movie_name = request.form['movieName']
+    top_similar_movies = recommend(movie_name)
+    return render_template('main.html', movie_name=movie_name, recommended_movies=top_similar_movies)
+
+if __name__ == '__main__':
+    app.run(debug=True)
